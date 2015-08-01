5b06a3291e23e3f9f34a43b9ba5392a0165d74f3
Navi-firm <navicorpdesigns@gmail.com>
2015-08-01 11:39:35 +0300

    MysqlTuts

---
+++ Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCheckConnection = New System.Windows.Forms.Button()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.lblUsername = New System.Windows.Forms.Label()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnSignin = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCheckConnection
+        '
+        Me.btnCheckConnection.Location = New System.Drawing.Point(74, 12)
+        Me.btnCheckConnection.Name = "btnCheckConnection"
+        Me.btnCheckConnection.Size = New System.Drawing.Size(123, 23)
+        Me.btnCheckConnection.TabIndex = 0
+        Me.btnCheckConnection.Text = "Check connection"
+        Me.btnCheckConnection.UseVisualStyleBackColor = True
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.Location = New System.Drawing.Point(71, 38)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(10, 13)
+        Me.lblMessage.TabIndex = 1
+        Me.lblMessage.Text = "."
+        '
+        'lblUsername
+        '
+        Me.lblUsername.AutoSize = True
+        Me.lblUsername.Location = New System.Drawing.Point(26, 66)
+        Me.lblUsername.Name = "lblUsername"
+        Me.lblUsername.Size = New System.Drawing.Size(55, 13)
+        Me.lblUsername.TabIndex = 2
+        Me.lblUsername.Text = "Username"
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(26, 90)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(53, 13)
+        Me.lblPassword.TabIndex = 3
+        Me.lblPassword.Text = "Password"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(87, 63)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(137, 20)
+        Me.txtUsername.TabIndex = 4
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(87, 90)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(137, 20)
+        Me.txtPassword.TabIndex = 5
+        '
+        'btnSignin
+        '
+        Me.btnSignin.Location = New System.Drawing.Point(29, 120)
+        Me.btnSignin.Name = "btnSignin"
+        Me.btnSignin.Size = New System.Drawing.Size(100, 23)
+        Me.btnSignin.TabIndex = 6
+        Me.btnSignin.Text = "Sign In"
+        Me.btnSignin.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnCancel.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.btnCancel.Location = New System.Drawing.Point(140, 121)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(83, 21)
+        Me.btnCancel.TabIndex = 7
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 166)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnSignin)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.lblUsername)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.btnCheckConnection)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCheckConnection As System.Windows.Forms.Button
+    Friend WithEvents lblMessage As System.Windows.Forms.Label
+    Friend WithEvents lblUsername As System.Windows.Forms.Label
+    Friend WithEvents lblPassword As System.Windows.Forms.Label
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnSignin As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,65 @@
+﻿
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    'Declare connection client
+    Dim MysqlConn As MySqlConnection
+    'ability to query the database
+    Dim COMMAND As MySqlCommand
+
+    'connection function
+    Sub Connection()
+
+        MysqlConn = New MySqlConnection
+        MysqlConn.ConnectionString = "server=localhost;userid=root;password=root;"
+
+        Try
+            MysqlConn.Open()
+        Catch ex As Exception
+            lblMessage.ForeColor = Color.Red
+            lblMessage.Text = "Connection Errors Try again"
+        Finally
+            MysqlConn.Dispose()
+        End Try
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCheckConnection.Click
+
+        'call the connection method
+        Connection()
+    End Sub
+
+
+    Private Sub btnSignin_Click(sender As Object, e As EventArgs) Handles btnSignin.Click
+        Connection()
+
+        'reader to the database
+        Dim READER As MySqlDataReader
+        Dim count As Integer = 0
+        Try
+            MysqlConn.Open()
+            Dim Query As String = "SELECT * FROM database.person where username='" & txtUsername.Text & "' AND password = '" & txtPassword.Text & "'"
+            COMMAND = New MySqlCommand(Query, MysqlConn)
+            READER = COMMAND.ExecuteReader
+
+            While READER.Read
+                count += 1
+            End While
+
+            'validate the inputs
+            If count = 1 Then
+                lblMessage.ForeColor = Color.Green
+                lblMessage.Text = "User found"
+            Else
+                lblMessage.ForeColor = Color.DarkRed
+                lblMessage.Text = "User not registered"
+            End If
+
+        Catch ex As Exception
+            lblMessage.Text = ex.Message
+
+        End Try
+    End Sub
+End Class
